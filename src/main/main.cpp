f7dc737dc389c6925632d5c4ee29ab409d2c118e
csix <cody.six@etudiant.univ-lr.fr>
2024-10-05 19:47:57 +0200

    ça bouge

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,13 +1,29 @@
+#include <chrono>
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
 #include "soupe.hpp"
 
-int main() {
+// get system time in nanoseconds
+long long nanoTime() {
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(
+        std::chrono::high_resolution_clock::now().time_since_epoch()
+    ).count();
+}
 
+int main() {
+    // gameLoop attributes
+    const int FPS = 60;
+    double renderInterval = 1000000000 / FPS;
+    double delta = 0;
+    long long lastTime = nanoTime();
+    long long currentTime;
+
+    // classes test
     Soupe soupe("soupe");
     std::cout << soupe.getName() << std::endl;
 
+    // textures and sprites test
     sf::Texture texture;
     if (!texture.loadFromFile("assets/mecha_arbre.png")) {
         std::cerr << "Error while trying to load image" << std::endl;
@@ -22,7 +38,10 @@ int main() {
     float width = dimensions.width;
     float heigt = dimensions.height;
 
-    sf::RenderWindow window(sf::VideoMode(width, heigt), "Super fenetre");
+    int spriteDirection = 1;
+
+    // renderwindow test
+    sf::RenderWindow window(sf::VideoMode(width * 2, heigt), "Super fenetre");
 
     while (window.isOpen()) {
         sf::Event event;
@@ -33,9 +52,32 @@ int main() {
             }
         }
 
-        window.clear();
-        window.draw(sprite);
-        window.display();
+        // gameLoop management
+        currentTime = nanoTime();
+        delta += (currentTime - lastTime) / renderInterval;
+        lastTime = currentTime;
+
+        if (delta >= 1) {
+            // sprite movement management
+            switch (spriteDirection) {
+            float coords[2];
+            case -1:
+                coords[0] = sprite.getPosition().x;
+                coords[1] = sprite.getPosition().y;
+                sprite.setPosition(coords[0] - 0.2, coords[1]);
+                if (coords[0] <= 0) spriteDirection = 1;
+                break;
+            case 1:
+                coords[0] = sprite.getPosition().x;
+                coords[1] = sprite.getPosition().y;
+                sprite.setPosition(coords[0] + 0.2, coords[1]);
+                if (coords[0] >= width) spriteDirection = -1;
+                break;
+            }
+            window.clear();
+            window.draw(sprite);
+            window.display();
+        }
     }
     return 0;
 }
